0284eb0f82f525d76d980ea62208c1330a4b34d1
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-04-04 19:55:26 +0100

    renemed reducer

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,12 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import css from "./SearchBox.module.css";
-import { filterContacts, selectNameFilter } from "../../redux/filtersSlice";
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
 
 const SearchBox = () => {
   const filter = useSelector(selectNameFilter);
   const dispatch = useDispatch();
   const handleSearch = (ev) => {
-    dispatch(filterContacts(ev.target.value));
+    dispatch(changeFilter(ev.target.value));
   };
   return (
     <label className={css.SearchBox}>
